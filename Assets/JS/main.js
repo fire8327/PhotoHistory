71ac005d5392ce237dc9a761488132ea18bb0413
fire8327 <fire83274@gmail.com>
2024-06-06 17:24:32 +0300

    create footer

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -4,3 +4,16 @@ $("#toggler").click(() => {
     $("#menu").toggleClass("max-lg:top-full")
     $("body").toggleClass("overflow-hidden")
 })
+
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        $("#to-top-button").removeClass("hidden");
+    } else {
+        $("#to-top-button").addClass("hidden");
+    }
+}
+
+function goToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
