8640c0e56a22125ec90ad9cfc422010534494d8b
fire8327 <fire83274@gmail.com>
2024-06-08 00:08:33 +0300

    create reviews

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -6,6 +6,7 @@ $("#toggler").click(() => {
 })
 
 
+/* кнопка скролла */
 window.onscroll = function () {
     if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
         $("#to-top-button").removeClass("hidden");
@@ -17,3 +18,16 @@ window.onscroll = function () {
 function goToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
+
+
+/* отзывы поп'ап */
+$("#popupToggler").click(() => {
+    $("#popupOverlay").toggleClass("-translate-x-full")
+    $("#popup").toggleClass("translate-x-[3000px]")
+    $("#popup").toggleClass("-translate-x-1/2 left-1/2")
+})
+$("#popupOverlay").click(() => {
+    $("#popupOverlay").addClass("-translate-x-full")
+    $("#popup").removeClass("-translate-x-1/2 left-1/2")
+    $("#popup").addClass("translate-x-[3000px]")
+})
